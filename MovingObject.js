d9da11e83f762f0da9ae16dc8d434907b26c05af
Clement Pigeon / Jonathan Potter <clementpigeon@gmail.com / jonathan.potter.business@gmail.com>
2013-09-24 11:18:39 -0400

    creates moving object skeleton

---
+++ MovingObject.js
@@ -0,0 +1,17 @@
+function MovingObject (pos,vel) {
+  this.pos = pos;
+  this.vel = vel;
+}
+
+MovingObject.prototype.move () {
+
+}
+
+MovingObject.prototype.draw (ctx) {
+
+}
+
+MovingObject.prototype.isCollidedWith (otherObject) {
+
+}
+
